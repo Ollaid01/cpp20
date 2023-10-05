28c5bd34d6c69acde94b444b0a3067f84aa9b844
madeWithHands <abby.xg.riff.in@gmail.com>
2023-10-06 01:50:37 +0200

    WIP - if statement and bool value

---
+++ fig05.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file fig05.cpp
+ * @author your name (you@domain.com)
+ * @brief if statement and bool value
+ * @version 0.1
+ * @date 2023-10-06
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int studentGrade{90};
+
+    if(studentGrade >= 60) {
+        cout << "Passed" << endl;
+    }
+
+    return 0;
+}
