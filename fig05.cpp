871c723ef9352ddaf75fe3f66cc04a7f483d0950
madeWithHands <abby.xg.riff.in@gmail.com>
2023-10-06 01:52:31 +0200

    WIP - if/else double selection statement

--- fig05.cpp
+++ fig05.cpp
@@ -1,7 +1,7 @@
 /**
  * @file fig05.cpp
  * @author your name (you@domain.com)
- * @brief if statement and bool value
+ * @brief if/else double selection statement
  * @version 0.1
  * @date 2023-10-06
  * 
@@ -14,10 +14,13 @@ using namespace std;
 
 int main() {
 
-    int studentGrade{90};
+    int studentGrade{30};
 
     if(studentGrade >= 60) {
         cout << "Passed" << endl;
+    }
+    else {
+        cout << "Failed" << endl;
     }
 
     return 0;
