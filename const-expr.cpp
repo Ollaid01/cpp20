97922e75adada49de4356ef373278d90a7cb8ae6
madeWithHands <abby.xg.riff.in@gmail.com>
2023-10-18 10:43:37 +0200

    wip - using constexpr, external and internal iteration.

--- const-expr.cpp
+++ const-expr.cpp
@@ -6,22 +6,39 @@ using namespace std;
 int main() {
 
     // create array size variable
-    constexpr int arraySize{5};
+    constexpr size_t arraySize{5};
 
     // create array with arraySize element and default initialize it
     array<int, arraySize> items{};
 
     // using range-based for for initialize items array
-    for(int& item : items) {
-        item = 2 + 2 * item;
+    // for(int& item : items) {
+    //     item = 2 + 2 * item;
+    // }
+
+    // using external iteration for initialize array items
+    for(int i{0}; i < items.size(); ++i) {
+        items.at(i) = 2 + 2 + i;
     }
 
     // after modification : using range-based-for to print element of array items
-    for(const int& item :  items) {
+    // for(const int& item :  items) {
+    //     cout << item << " ";
+    // }
+
+    // print items using internal iteration (more secure)
+    for(const int& item : items) {
         cout << item << " ";
     }
 
-    cout << "\n";
+    cout << " \n ======== \n";
+
+    // print items using external iteration (less secure)
+    for(size_t i{0}; i < items.size(); ++i) {
+        cout << items.at(i) << " ";
+    }
+
+    cout << " \n ======== \n";
 
     return 0;
 }
