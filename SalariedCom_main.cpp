11bc7c610c8409029471db6fbe1778ffc4ad0067
madeWithHands <abby.xg.riff.in@gmail.com>
2023-10-31 01:00:36 +0100

    tips

--- SalariedCom_main.cpp
+++ SalariedCom_main.cpp
@@ -5,26 +5,32 @@
 
 int main(int argc, char* argv[]) {
 
-    SalariedEmployee employee{"Marie V", 300.45};
-    cout << employee.toString() << endl;
+    // SalariedEmployee employee{"Marie V", 300.45};
+    // cout << employee.toString() << endl;
 
-    cout << " =========== " << endl;
+    // cout << " =========== " << endl;
 
-    SalariedComEmployee employee2{"Marie V", 300.45, 125, 0.3};
-    employee2.setName("Mathieu D.");
-    employee2.setSalary(130);
-    employee2.setGrossSales(206);
-    employee2.setComissions(0.8);
-    cout << employee2.toString() << endl;
+    SalariedComEmployee salariedComission{"Marie V", 300.45, 125, 0.3};
+    // salariedComission.setName("Mathieu D.");
+    // salariedComission.setSalary(130);
+    // salariedComission.setGrossSales(206);
+    // salariedComission.setComissions(0.8);
+    // cout << salariedComission.toString() << endl;
 
-    cout << "========= \n Natural Pointer \n========= " << endl;
+    // cout << "========= \n Natural Pointer \n========= " << endl;
 
-    // natural : aim a base-class pointer at derived-class object
-    SalariedEmployee* salariedPtr{&employee2};
-    cout << fmt::format("Name (Ptr) : {}", salariedPtr->getName()) << endl;
-    salariedPtr->setName("Roxane M");
-    cout << fmt::format("Name (Ptr) : {}", salariedPtr->getName()) << endl;
-    cout << salariedPtr->toString() << endl;
+    // natural : aim a base-class pointer at derived-class object (allowed)
+    SalariedEmployee* salariedPtr{&salariedComission};
+
+    // invoke base-class member functions on derivated class object through base-class pointer (allowed)
+    string name{salariedPtr->getName()};
+    double salary{salariedPtr->getSalary()};
+    // double grossSales{salariedPtr->getGrossSales()}: disallow 
+
+    // cout << fmt::format("Name (Ptr) : {}", salariedPtr->getName()) << endl;
+    // salariedPtr->setName("Roxane M");
+    // cout << fmt::format("Name (Ptr) : {}", salariedPtr->getName()) << endl;
+    // cout << salariedPtr->toString() << endl;
 
     // Not allowed: a derived-class pointer at baed-class object
 
