04f05ef9403e4d5c7cb34542f6438eb66c519cc5
madeWithHands <abby.xg.riff.in@gmail.com>
2023-11-01 15:34:41 +0100

    method virtuel pure, interface, classe abstraite

---
+++ fn_virtuel_pure.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class Animal {
+
+    public:
+        virtual void speak() = 0; // functionn virtuel pure qui fait Animal devient automatiquement abstraite
+};
+
+class Chien : public Animal {
+
+    public:
+    void speak() override {
+        std::cout << "Woof!" << std::endl;
+    }
+};
+
+class Chat: public Animal {
+
+    public:
+    void speak() override {
+        std::cout << "Meow Meow !" << std::endl;
+    }
+};
+
+int main(int argc, char* argv[]) {
+
+    // Animal myAnimal; // not allowed, compilation error (error: cannot declare variable ‘myAnimal’ to be of abstract type ‘Animal’)
+
+    Chien myDogInstance;
+    Animal* myDog{&myDogInstance}; // alowed and choice right version of speak function (chien)
+    myDog->speak();
+
+    Chat myChatInstance;
+    Animal* myChat{&myChatInstance}; // alowed and choice right version of speak function (chat)
+    myChat->speak();
+
+    return 0;
+}
